6685da804f4e648cd29eea4fd1c52e89ea7171fe
de France <52699516+MdeFrance@users.noreply.github.com>
2019-08-30 15:09:33 +0000

    Fixed Broken Code

--- Multiplication_Table_Recaller.py
+++ Multiplication_Table_Recaller.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 correct = 0
 incorrect = 0
@@ -8,39 +9,57 @@ print('=' * 30)
 print('Type "quit" to see your grade!')
 print('=' * 30)
 
+
+def exit():
+    print('\n\nThank you for trying!')
+    time.sleep(3)
+    quit()
+
+
+# DRY.. Make table and vector into one function which you use twice.
 try:
     # Promts user to choose which table to recall from memory
-    table = str(input('\nWhich multiplication table to recall? :> ').lower())
+    table = input('\nWhich multiplication table to recall? :> '.lower())
 
-    if table == 'quit' or table == 'result':
+    try:
+        if table == 'quit' or table == 'result':
+            raise KeyboardInterrupt
+
+        table = int(table)
+
+        if table in range(0, 1001):
+            # Convert the string table into a interger
+            table = int(table)
+        elif int(table) not in range(0, 1001):
+            print('\nSorry, I won\'t produces numbers higher then 1000, Smarty-Pants.')
+            exit()
+    except ValueError:
+        print("\nThat's not a number!\n")
         raise KeyboardInterrupt
-    elif int(table) in range(1, 1001):
-        table = int(table)          # Convert the string table into a interger
-    elif int(table) not in range(1, 1001):
-        print('\nSorry, I won\'t produces numbers higher then 1000, Smarty-Pants.')
-        exit(0)
-    else:
-        print("That's not a number!")
-        exit(0)
 
     # Prompts user to choose up to which vector they want to practice.
     vector = str(input('Up to which vector? :> ').lower())
 
-    if vector == 'quit' or vector == 'result':
+    try:
+        if vector == 'quit' or vector == 'result':
+            raise KeyboardInterrupt
+
+        vector = int(vector)
+
+        if vector in range(0, 1001):
+            # Convert the string vector into a interger
+            vector = int(vector)
+        elif int(vector) not in range(0, 1001):
+            print('\nSorry, I won\'t produces numbers higher then 1000, Smarty-Pants.')
+    except ValueError:
+        print("\nThat's not a number!\n")
         raise KeyboardInterrupt
-    elif int(vector) in range(1, 1001):
-        vector = int(vector)     # Convert the string 'vector' into a interger.
-    elif int(vector) not in range(1, 1001):
-        print('\nSorry, I won\'t produces numbers higher then 1000, Smarty-Pants.')
-        exit(0)
-    else:
-        print("That's not a number!")
-        exit(0)
 
 except KeyboardInterrupt:
-    print('\n\nThank you for trying!\n')
-    exit(0)
+    exit()
 
+if vector == 0 or table == 0:
+    exit()
 
 text = (f'\n\n\nRecalling the table of {table} with a vector of {vector}.')
 print(text)
@@ -52,6 +71,7 @@ while True:
     # xx Creates a random number to switch the questions.
     x = random.randint(1, vector)
     xx = random.randint(1, 3)
+
     if xx == 1:
         print(f'\n\nWhat is {x} * {table}?')
     else:   # 1 seems more dominant. xx gives this line more chances.
@@ -60,27 +80,34 @@ while True:
     sum = x * table                         # Calculate the correct answer.
 
     try:
-        answer = str(input(':> '))          # Converters answer to a string
-        if answer == "":                    # for error handling purposes.
+        try:
+            answer = str(input(':> '))          # Converters answer to a string
+
+            if answer == 'quit' or answer == 'result':
+                raise KeyboardInterrupt
+            elif answer == "":                    # for error handling purposes.
+                answer = 0
+
+            answer = int(answer)                # Converters answer back to a integer
+
+            if int(answer) not in range(0, 1001):
+                print(
+                    '\nSorry, I won\'t produces numbers higher then 1000, Smarty-Pants.')
+        except ValueError:
             answer = 0
-        elif int(answer) not in range(1, 1001):
-            print('That is not a numer')
-        answer = int(answer)                # Converts answer to an interger.
 
     except KeyboardInterrupt:
         try:
             print('\nYour grade is a', int(correct * 10 / total), '/ 10\n')
-            print('\n\nThank you for trying!\n')
-            exit(0)
+            exit()
         except ZeroDivisionError:
-            exit(0)
+            exit()
 
     if answer == sum:
         print('\nThat\'s correct!')
         print('=' * 15)                         # Highlights the right anwer.
         correct += 1
         total += 1
-
     else:
         print('\nThat\'s not correct!')
         print('Correct answer is', sum)
