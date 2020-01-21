26bebe309b11143d9577ec55a5051c5554cd2565
MdeFrance <mdefrance1988@gmail.com>
2020-01-21 19:06:34 +0100

    Add section File Handeling

--- Python_Exam_Maker/main.py
+++ Python_Exam_Maker/main.py
@@ -10,12 +10,14 @@ scriptName = base.getScriptName()
 
 addQuestion = {}
 addAnswers = {}
+answerYes = ('Y', 'y', 'YES', 'YEs', 'Yes', 'yEs', 'yeS', 'yes')
+answerNo = ('N', 'NO', 'n', 'No', 'nO')
 
 # Clears screen
 base.sys_clear()
 
 # Create instance of class myExam
-obj = base.myExam()
+obj = base.MyExam()
 
 # Show how to use and recommendation on how to use properly.
 # Like if you create a new exam, try to make it about one topic only (classExam.txt or stringManipulation)
@@ -26,27 +28,62 @@ obj = base.myExam()
 
 # Use class methode to add a question to dict: 'addQuestion'
 try:
+    alreadySavedToFile = 0
+
     while True:
+
         addQuestion[len(
             addQuestion) + 1] = obj.addNew(input('\n\tAdd Exam Question\n\n:> '), input('\n\n\tAdd Exam Answer\n\n:> '))
 
+        # Clears screen
         base.sys_clear()
 
         print('Question:\t', addQuestion[(
             len(addQuestion))][0], '\nAnswer:\t\t', addQuestion[(len(addQuestion))][1])
 
-        answer = input('\n\n\tAdd another? (Y/N) :> ').capitalize()
+        # File handeling
 
-        if answer in ('Y', 'y', 'YES', 'YEs', 'Yes', 'yEs', 'yeS', 'yes'):
+        while True:
+
+            answer = input('\n\tSave Q and A to file? (Y/N) :> ')
+
+            if answer in answerYes:
+                if alreadySavedToFile == 0:
+                    alreadySavedToFile += 1
+                    base.saveToFile(True, addQuestion, alreadySavedToFile)
+                else:
+                    base.appendToFile(True, addQuestion, alreadySavedToFile)
+
+                try:
+                    print("\tExam Question {}:\tSaved!".format(alreadySavedToFile))
+                except Exception as e:
+                    print("Exam Question{}:\tNot Saved!".format(alreadySavedToFile))
+                    time.sleep(2)
+                    logging.error(traceback.format_exc())
+
+                time.sleep(2)
+                break
+            elif answer in answerNo:
+                break
+            else:
+                print("\n\t'" + answer + "' is not a valid input!")
+                time.sleep(3)
+                continue
+
+        answer = input('\nAdd another Q and A? (Y/N) :> ')
+
+        # Variable also used to count the number of question added.
+        alreadySavedToFile += 1
+
+        if answer in answerYes:
             base.sys_clear()
             continue
-        elif answer in ('N', 'NO', 'n', 'No', 'nO'):
+        elif answer in answerNo:
             input('\nPress enter to quit')
             break
         else:
-            print("\t'" + answer + "' is not a valid input!")
+            print("\n\t'" + answer + "' is not a valid input!")
             time.sleep(3)
-            base.sys_clear()
             continue
 
 except Exception as e:
