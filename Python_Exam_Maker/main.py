4c582fc7bac6f90200089674b5f28e3069bcea5f
MdeFrance <mdefrance1988@gmail.com>
2020-02-01 00:45:32 +0000

    Update main.py

--- Python_Exam_Maker/main.py
+++ Python_Exam_Maker/main.py
@@ -13,9 +13,10 @@ menuOption = base.startMenu(parent_Folder_Path)
 
 # import os
 
-# os.chdir(f'{os.getcwd()}/My_Exams/3')
+# os.chdir(f'{os.getcwd()}/My_Exams/')
+# os.mkdir('Test_For_Delete_Option_3')
 
-# num = 101
+# num = 25
 # for i in range(1, num):
 #     with open(f'{i}.py', 'w+', encoding="utf-8") as f:
 #         string = 'print'
