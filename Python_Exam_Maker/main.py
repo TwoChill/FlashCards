00a27b78864deccc7a6f4a0725329dd9d80319a7
MdeFrance <mdefrance1988@gmail.com>
2020-02-01 00:53:50 +0000

    Add Python Version Checker
    I used Python 3(.7.5)

--- Python_Exam_Maker/main.py
+++ Python_Exam_Maker/main.py
@@ -1,4 +1,8 @@
 import base
+import sys
+
+if sys.version_info[0] < 3:
+    raise Exception("Python 3 or a more recent version is required.")
 
 # Create a Main Folder
 parent_Folder_Path = base.createMain()
