5a125406525cb7f73723c0bee0b7da5c81b69d21
MdeFrance <mdefrance1988@gmail.com>
2020-02-04 22:12:09 +0100

    Update main.py

--- Python_Exam_Maker/main.py
+++ Python_Exam_Maker/main.py
@@ -5,8 +5,8 @@ if sys.version_info[0] < 3:
     raise Exception("Python 3 or a more recent version is required.")
 
 # Create a Main Folder
-parent_Folder_Name, parent_Folder_Path = base.createMain()
+main_Folder_Name, main_Folder_Path = base.createMain()
 
 # !! Currently Working On !!
 # Menu
-menuOption = base.startMenu(parent_Folder_Path)
+menuOption = base.startMenu(main_Folder_Path)
